0cebbefcb303efdeccc6b3b1d9f98093819ff04c
Alex Willisson <alex.willisson@gmail.com>
2021-03-11 22:49:41 +0000

    serves json of articles

--- dist/out-tsc/app.js
+++ dist/out-tsc/app.js
@@ -4,10 +4,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
+var articles_1 = require("./articles");
 var app = express_1.default();
 var port = 3000;
 app.get('/', function (req, res) {
-    res.send('The sedulous hyena ate the antelope!');
+    res.send(JSON.stringify(articles_1.articles));
 });
 app.listen(port, function () {
     console.info("Ready on port " + port);
--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 
+import { articles } from './articles';
+
 const app = express();
 const port = 3000;
 app.get('/', (req, res) => {
-    res.send('The sedulous hyena ate the antelope!');
+    res.send(JSON.stringify(articles));
 });
 app.listen(port, () => {
     console.info(`Ready on port ${port}`);
