3dbc36c73d23c5d567dfa985888ed6644ea099c5
Alex Willisson <alex.willisson@gmail.com>
2021-03-12 20:40:32 +0000

    removes unused imports

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,5 @@
 import express from 'express';
-import http from 'http';
 import fetch from 'node-fetch';
-import { ExceptionHandler } from 'winston';
 
 import { Parser as xmlParser } from 'xml2js';
 
